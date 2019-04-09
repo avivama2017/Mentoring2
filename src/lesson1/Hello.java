c0e899d0d6f2b0d51d271fd54dbf0a2fa7a6a263
md riad <mdriad@mds-MacBook-Pro.local>
2019-04-09 17:53:36 -0400

    mentoring2

---
+++ src/lesson1/Hello.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class Hello {
+	
+	public static void main(String []args) {
+
+		int a = 10;
+		 
+		if (a == 10); 
+		System.out.println( " A equal 10");
+		
+		if (a>= 10);
+		System.out.println( "A is a big number");
+		if (a <= 10);
+		System.out.println( "A is a small number");
+		}
+	}		
+		
+		
+	
